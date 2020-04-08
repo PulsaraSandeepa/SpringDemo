aa46d94e09d12bf3b23005ca07ff745f5f2f0571
Pulsara Sandeepa <pulsarasandeepa@gmail.com>
2020-04-08 17:45:41 +0530

    basics

--- src/main/java/com/example/demo/BookResource.java
+++ src/main/java/com/example/demo/BookResource.java
@@ -2,8 +2,7 @@ package com.example.demo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,18 @@ public class BookResource {
     private BookService bookService;
 
     @GetMapping("/books")
-    public List<Book> getAllBooks(){
+    public List<Book> getAllBooks() {
         return bookService.findAllBooks();
+    }
+
+    @GetMapping("/books/{bookId}")
+    public Book retrieveBook(@PathVariable int bookId){
+        return bookService.findBook(bookId);
+    }
+
+    @PostMapping("/books")
+    public void createBook(@RequestBody Book book){
+        Book addBook =bookService.addBook(book);
     }
 
 }
